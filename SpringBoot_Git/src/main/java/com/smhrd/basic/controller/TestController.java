0574b931a171ca5c0ed53ac310d35f60a8310550
smhrd <smhrd@192.168.219.169>
2025-03-13 12:27:36 +0900

    asd

--- SpringBoot_Git/src/main/java/com/smhrd/basic/controller/TestController.java
+++ SpringBoot_Git/src/main/java/com/smhrd/basic/controller/TestController.java
@@ -3,11 +3,11 @@ package com.smhrd.basic.controller;
 public class TestController {
 	// 이선홍 덱스 인가.....덱스 선스틱의 효과는????
 	
-<<<<<<< HEAD
+
 	// asdfsadfsdafsadfsdafasdfsdafsa
 	
-=======
+
 	// 오로어 이ㅏㅓ링
->>>>>>> branch 'master' of https://github.com/2024-SMHRD-KDT-BigData-29/Saito-Takeda.git
+
 	
 }
