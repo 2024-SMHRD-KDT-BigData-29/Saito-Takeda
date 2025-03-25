7a0440e9b2532dd3c8ec0222498fabe97c40c6a8
smhrd <smhrd@192.168.219.95>
2025-03-25 15:20:35 +0900

    dddd

--- SpringBoot_Git/src/main/java/com/smhrd/basic/entity/UserEntity.java
+++ SpringBoot_Git/src/main/java/com/smhrd/basic/entity/UserEntity.java
@@ -1,6 +1,8 @@
 package com.smhrd.basic.entity;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,40 +18,37 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Table(name = "TB_USER")
+@Table(name = "tb_user")
 public class UserEntity {
     
     @Id
-    @Column(name = "USER_EMAIL", nullable = false)
+    @Column(name = "user_email", nullable = false, length = 50)
     private String userEmail;
     
-    @Column(name = "USER_PW", nullable = false, length = 255)
+    @Column(name = "user_pw", nullable = false, length = 255)
     private String userPw;
     
-    @Column(name = "USER_NAME", nullable = false, length = 100)
+    @Column(name = "user_name", nullable = false, length = 50)
     private String userName;
     
-    @Column(name = "USER_PHONE", nullable = true, length = 20)
+    @Column(name = "user_phone", nullable = true, length = 20)
     private String userPhone;
     
-    @Column(name = "USER_GENDER", nullable = true, length = 1)
+    @Column(name = "user_gender", nullable = true, length = 1)
     private String userGender;
     
-    @Column(name = "USER_REGION", nullable = true, length = 4)
+    @Column(name = "user_region", nullable = true, length = 14)
     private String userRegion;
     
-    @Column(name = "USER_ADDR", nullable = true, length = 500)
+    @Column(name = "user_addr", nullable = true, length = 500)
     private String userAddr;
     
-    @Column(name = "USER_STATUS", nullable = true, length = 10)
+    @Column(name = "user_status", nullable = true, length = 10)
     private String userStatus;
     
-    @Column(name = "USER_ROLE", nullable = true, length = 1)
+    @Column(name = "user_role", nullable = true, length = 1)
     private String userRole;
     
-    @Column(name = "JOINED_AT", nullable = true)
+    @Column(name = "joined_at", nullable = true)
     private Timestamp joinedAt;
 }
