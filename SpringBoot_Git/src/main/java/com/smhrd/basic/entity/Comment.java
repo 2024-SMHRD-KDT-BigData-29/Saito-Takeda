e8e3c3c8cacdb447bf6ba3dc17ee69406e11bda5
smhrd <smhrd@192.168.219.95>
2025-03-25 11:20:08 +0900

    성규

--- SpringBoot_Git/src/main/java/com/smhrd/basic/entity/Comment.java
+++ SpringBoot_Git/src/main/java/com/smhrd/basic/entity/Comment.java
@@ -3,8 +3,6 @@ package com.smhrd.basic.entity;
 import java.sql.Timestamp;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
